fbec09c497d389bd2756c332626dcd12b4d07506
bush <luckydady09@gmail.com>
2013-05-27 17:10:45 +0900

    5/27

--- java/controller/HelloWorldController.java
+++ java/controller/HelloWorldController.java
@@ -1,5 +1,8 @@
 package controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,6 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HelloWorldController {
 
+	//private Logger logger = Logger.getLogger(getClass());
 	
 	@RequestMapping("/hello")
 	public ModelAndView helloWorld(){
@@ -19,6 +23,16 @@ public class HelloWorldController {
 		mav.addObject("message", message);
 		
 		return mav;
+	}
+	
+	@RequestMapping("/search")
+	public String search(HttpServletRequest request, HttpServletResponse response){
+		
+		String keyword = request.getParameter("keyword");
+		
+		//logger.info(keyword);
+		
+		return keyword;
 	}
 	
 }
--- webapp/WEB-INF/jsp/hello.jsp
+++ webapp/WEB-INF/jsp/hello.jsp
@@ -5,8 +5,28 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
 <title>springstudy</title>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" ></script>
+<script>
+	$(document).ready(function(){
+		//$("#bush").text("haha");
+		$(document).on("click","#ajaxadd",function(){
+			var temp = "<ul><input type='text' name='ajaxname' /><input type='button' class='ajaxminus' value='-' /></ul>";
+			$("#ajaxtable").append(temp);
+		});
+		
+		$(document).on("click",".ajaxminus",function(){
+			$(this).prev().remove();
+		});
+	});
+</script>
 </head>
 <body>
 	${message}
+	<div id="bush">
+		<ul>
+			<input type="button" id="ajaxadd" value="추가" />
+		</ul>
+	</div>
+	<div id="ajaxtable"></div>
 </body>
 </html>
